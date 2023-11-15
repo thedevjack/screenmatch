c2e7ecf23a2eaf7f4038f45f797ff16ca6215776
thedevjack <jackson1santos@hotmail.com>
2023-11-15 17:17:14 -0300

    Incluindo desafio minhasmusicas

---
+++ minhasmusicas/src/br/com/alura/minhasmusicas/model/Podcast.java
@@ -0,0 +1,24 @@
+package br.com.alura.minhasmusicas.model;
+
+// SUB CLASSE que HERDA de -> Audio
+public class Podcast extends Audio{
+
+    private String host;
+    private String descricao;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
