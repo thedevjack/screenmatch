23919a34f553666f89edd0c42b67f2563ea90ade
Jackson santos <jackson1santos@hotmail.com>
2023-12-14 00:11:21 -0300

    refinando a busca por filmes

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,14 +5,28 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um filme p/ busca: ");
+        var busca = scanner.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=2a58e946";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=top+gun&apikey=2a58e946"))
+                .uri(URI.create(endereco))
                 .build();
+
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
+
     }
+
 }
